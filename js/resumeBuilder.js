7d699bbce577af7717c51af1bf0c22ddfa3c9c51
danielbales <danielfbales@gmail.com>
2016-03-14 10:17:49 -0700

    style: added header JS

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "Daniel Bales";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Ninja";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+console.log(formattedName);
