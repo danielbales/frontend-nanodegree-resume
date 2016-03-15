61bc4b56952f93e3536853215d984f7cc54d2479
danielbales <danielfbales@gmail.com>
2016-03-14 21:58:41 -0700

    style: added contact info and work experience section.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,7 +4,26 @@ var formattedName = HTMLheaderName.replace("%data%", name);
 var role = "Ninja";
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 
+var email = "danielfbales@gmail.com";
+var formattedEmail = HTMLemail.replace("%data%", email);
+
+var github = "danielbales";
+var formattedGithub = HTMLgithub.replace("%data%", github);
+
+var mobile = "+01 858 414 2528";
+var formattedMobile = HTMLmobile.replace("%data%", mobile);
+
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+$('#topContacts').prepend(formattedEmail);
+$('#topContacts').prepend(formattedGithub);
+$('#topContacts').prepend(formattedMobile);
+
 
 console.log(formattedName);
+
+
+
+
+
+$('#workExperience').append(HTMLworkStart);
